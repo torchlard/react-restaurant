f59b26785bda450ead4474a0c5d7a5265afc1fe3
torchlard <kidult.family@gmail.com>
2019-04-10 22:19:01 +0800

    new

--- src/Table.js
+++ src/Table.js
@@ -19,7 +19,21 @@ class Table extends Component {
   render(){
     return (
       <div>
-
+        <table>
+          <tr>
+            <th>Table No</th>
+            <th>Num Of Seat</th>
+            <th>Availability</th>
+          </tr>
+          { this.state.tables.map(item => (
+                <tr>
+                  <td>{item.tableNo}</td>
+                  <td>{item.numOfSeat}</td>
+                  <td>{item.available}</td>
+                </tr>
+              )
+            )}
+        </table>
       </div>
     )
   }
